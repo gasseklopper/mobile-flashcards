bb3e15f95ce6e588c07981e66fc85f758eb66dd2
gasseklopper <m-morley@gmx.de>
2019-08-03 21:21:39 +0200

    added redux thunk navigation

---
+++ components/AddEntry.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+import { getMetricMetaInfo } from "../utils/helpers";
+
+export default class AddEntry extends Component {
+  render() {
+    return <View>{getMetricMetaInfo("bike").getIcon()}<Text>hello</Text></View>;
+  }
+}
--- utils/colors.js
+++ utils/colors.js
@@ -2,7 +2,7 @@
 
 export const purple = '#292477'
 export const gray = '#757575'
-export const white = '#fff'
+export const white = '#757575'
 export const red = '#b71845'
 export const orange = '#f26f28'
 export const blue = '#4e4cb8'
