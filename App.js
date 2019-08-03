67b21f64cc23fc7ebfa551665b3186505c1d87a1
gasseklopper <m-morley@gmx.de>
2019-08-03 21:03:25 +0200

    add utils from fitness app to get local envoirement running

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+          <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+        </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
