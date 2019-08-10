de921b3318a3ef941490b7253a13ac995757e9af
gasseklopper <m-morley@gmx.de>
2019-08-10 07:40:02 +0200

    Added data for the project as an object

---
+++ utils/_DATA.js
@@ -0,0 +1,44 @@
+export const decks = {
+	React: {
+		title: 'React',
+		questions: [
+			{
+				question: 'What is React?',
+				answer: 'A library for managing user interfaces'
+			},
+			{
+				question: 'Where do you make Ajax requests in React?',
+				answer: 'The componentDidMount lifecycle event'
+			}
+		]
+	},
+	JavaScript: {
+		title: 'JavaScript',
+		questions: [
+		{
+			question: 'What is a closure?',
+			answer:
+			'The combination of a function and the lexical environment within which that function was declared.'
+		}
+		]
+	},
+	html: {
+		title: 'Redux',
+		questions: [
+		{
+			question: 'What is Html',
+			answer: 'Markup language'
+		},
+		{
+			question: 'What is a span',
+			answer:
+			'its an node element '
+		},
+		{
+			question: 'What is a CSS?',
+			answer:
+			'Cascading style sheets'
+		}
+		]
+	}
+}
