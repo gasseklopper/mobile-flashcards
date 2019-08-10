f209c78c861b56fa0709f09144714ac8243f0146
gasseklopper <m-morley@gmx.de>
2019-08-10 18:14:18 +0200

    Added reducer RECEIVE_DECKS,ADD_DECK,ADD_CARD

--- reducers/index.js
+++ reducers/index.js
@@ -1,20 +1,36 @@
-import { RECEIVE_ENTRIES, ADD_ENTRY } from '../actions'
-// added reduceefr from https://github.com/udacity/reactnd-UdaciFitness-complete/blob/reducers/components/AddEntry.js
-function entries (state = {}, action) {
-  switch (action.type) {
-    case RECEIVE_ENTRIES :
-      return {
-        ...state,
-        ...action.entries,
-      }
-    case ADD_ENTRY :
-      return {
-        ...state,
-        ...action.entry
-      }
-    default :
-      return state
-  }
-}
+import {
+	RECEIVE_DECKS,
+	ADD_DECK,
+	ADD_CARD
+} from '../actions/index'
+import { decks as INITIAL_STATE } from '../utils/_DATA'
 
-export default entries
+export default function decks(state = {}, action) {
+	switch (action.type) {
+		case RECEIVE_DECKS:
+			return {
+				...state,
+				...action.decks
+			}
+		case ADD_DECK:
+			const { title } = action;
+			return {
+				...state,
+				[title]: {
+					title,
+					questions: []
+				}
+			}
+		case ADD_CARD:
+			const { deckId, card } = action
+			return {
+				...state,
+				[deckId]: {
+					...state[deckId],
+					questions: [...state[deckId].questions].concat(card)
+				}
+			}
+		default:
+			return state
+	}
+}
