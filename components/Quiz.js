f395b5313f19e8a474e99eb0b7b2b7d3ab6cc11b
gasseklopper <m-morley@gmx.de>
2019-08-10 22:57:33 +0200

    added quiz component for selecting ios oro android

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import Constants from 'expo-constants'
+import Quiz_Android from './Quiz_Android'
+import Quiz_iOS from './Quiz_iOS'
+// import { setLocalNotification, clearLocalNotification } from '../utils/helpers'
 
-class Quiz extends Component {
+export class Quiz extends Component {
+	static navigationOptions = ({ navigation }) => {
+		const title = navigation.getParam('title', '')
+		return {
+			title: `${title} Quiz`
+		}
+	}
+
+	componentDidMount() {
+		// clearLocalNotification().then(setLocalNotification)
+	}
+
 	render() {
-		return (
-			<View>
-				<Text>Quiz Screen</Text>
-			</View>
-		)
+		const { navigation } = this.props
+		const title = navigation.getParam('title', '')
+
+		if (Constants.platform.android) {
+			return <Quiz_Android title={title} />
+		}
+		return <Quiz_iOS title={title} />
 	}
 }
 
